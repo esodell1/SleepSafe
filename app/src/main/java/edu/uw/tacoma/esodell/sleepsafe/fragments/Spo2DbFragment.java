7048328ab48533ca358d755e0eceacced9102934
Eric Odell <esodell@uw.edu>
2016-04-19 14:20:06 -0700

    Initial commit

---
+++ app/src/main/java/edu/uw/tacoma/esodell/sleepsafe/fragments/Spo2DbFragment.java
@@ -0,0 +1,31 @@
+package edu.uw.tacoma.esodell.sleepsafe.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tacoma.esodell.sleepsafe.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Spo2DbFragment extends Fragment {
+
+
+    public Spo2DbFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_spo2_db, container, false);
+    }
+
+}
