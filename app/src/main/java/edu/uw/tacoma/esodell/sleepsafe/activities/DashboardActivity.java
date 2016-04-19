d85253814a764fd0d4a296c0aaf1ad35686b3c90
Eric Odell <esodell@uw.edu>
2016-04-19 14:37:07 -0700

    Added menu

--- app/src/main/java/edu/uw/tacoma/esodell/sleepsafe/activities/DashboardActivity.java
+++ app/src/main/java/edu/uw/tacoma/esodell/sleepsafe/activities/DashboardActivity.java
@@ -1,15 +1,21 @@
 package edu.uw.tacoma.esodell.sleepsafe.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import edu.uw.tacoma.esodell.sleepsafe.R;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private View hrdb_fragment;
+    private View spo2db_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +32,43 @@ public class DashboardActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        hrdb_fragment = findViewById(R.id.hrdb);
+        hrdb_fragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), HrActivity.class));
+            }
+        });
+
+        spo2db_fragment = findViewById(R.id.spo2db);
+        spo2db_fragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "SpO2 clicked!", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_dashboard, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
