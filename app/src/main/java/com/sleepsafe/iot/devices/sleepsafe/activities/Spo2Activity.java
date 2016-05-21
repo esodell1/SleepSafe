b7e599e8defdfb637bfb9a896babbbf67c2bf062
Eric Odell <esodell@uw.edu>
2016-05-21 11:03:11 -0700

    Added dashboard banner

--- app/src/main/java/com/sleepsafe/iot/devices/sleepsafe/activities/Spo2Activity.java
+++ app/src/main/java/com/sleepsafe/iot/devices/sleepsafe/activities/Spo2Activity.java
@@ -1,5 +1,7 @@
 package com.sleepsafe.iot.devices.sleepsafe.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -57,30 +59,6 @@ public class Spo2Activity extends AppCompatActivity {
         }
 
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_spo2, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
